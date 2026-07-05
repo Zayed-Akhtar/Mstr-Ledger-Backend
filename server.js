62ec10faaab88d2525287f14484ef7a0dacbd50e
Zaid <zaid@Zaids-MacBook-Air.local>
2026-07-05 23:52:18 +0530

    Added mongodb connection via mongoose ORM

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
+const db = require("./config/mongodb-cpnnection");
 app.use(
   cors({
     origin: "http://localhost:5174",
