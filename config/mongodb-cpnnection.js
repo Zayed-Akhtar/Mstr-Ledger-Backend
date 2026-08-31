2176f4110a525b97119d18cebd32093d7fcac94e
Zaid <zaid@Zaids-MacBook-Air.local>
2026-08-31 23:47:55 +0530

    Monogo db connection test

--- config/mongodb-cpnnection.js
+++ config/mongodb-cpnnection.js
@@ -1,8 +1,12 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 mongoose.connect(process.env.MONGODB_URI)
-.then(()=>{
-    console.log('db connected');
-}).catch(err=> console.log(err));
+    .then(() => {
+        console.log("db connected");
+    })
+    .catch(err => {
+        console.error("MongoDB connection failed:");
+        console.error(err);
+    });
 
 module.exports = mongoose.connection;
